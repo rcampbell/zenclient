7f526abd8a58031f84c1516402dfbeb35af2728b
Robert Campbell <rrc@robert-campbell.com>
2011-01-10 17:14:04 +0100

    removed docs in favor of examples

--- src/zenclient/creating.clj
+++ src/zenclient/creating.clj
@@ -3,32 +3,7 @@
   (:use zenclient.core
 	[clojure.contrib.string :only (blank? lower-case)]))
 
-(defn create-job!
-  "Creates a new encoding job.
-
-   input string
-
-   A URI from which to download the input video.
-
-   options are key/value pairs:
-
-   :test boolean
-
-   Marks this as a test job. All jobs created under integration mode are test jobs.
-
-   :download-connections int
-
-   Specifies the number of connections used to download the input file.
-
-   :region string
-
-   Specifies the region in which processing.
-
-   :outputs [m]
-
-   Desired output files. Create with fn output.
-  "
-  [input & options]
+(defn create-job! [input & options]
   (let [job (merge {:api-key api-key :input input}
 		   (apply array-map options))]
     (api-post "/jobs" job)))
@@ -42,5 +17,6 @@
 (letfn [(options-map [& options] (apply array-map options))]
   (def output options-map)
   (def watermark options-map)
-  (def thumbnails options-map))
+  (def thumbnails options-map)
+  (def access-control options-map))
 
