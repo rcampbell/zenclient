b3147ef362dd973f2848844ddbd3e5dd3a72938b
Robert Campbell <rrc@robert-campbell.com>
2011-01-13 10:20:50 +0100

    minor cleanup

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject zenclient "1.1"
   :description "A simple Clojure DSL for the Zencoder API"
+  :url "https://github.com/rcampbell/zenclient"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [joda-time/joda-time "1.6.2"]]
