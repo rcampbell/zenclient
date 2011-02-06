4025ed26c0d329ad51e1830ba3df3c56d3c154f3
Robert Campbell <rrc@robert-campbell.com>
2011-02-06 19:37:32 +0100

    added support for raw evals of the DSL

--- src/zenclient/java.clj
+++ src/zenclient/java.clj
@@ -10,18 +10,26 @@
 	   :constructors {[String] []}
 	   :init init
 	   :state apiKey
-	   :methods [[createJob [String] java.util.Map]
+	   :methods [[eval [String] Object]
+		     [createJob [String] java.util.Map]
 		     [createJob [String String] java.util.Map]
 		     [id [Object] long]
+		     [state [Object] String]
 		     [notification [String] java.util.Map]
 		     [job [java.util.Map] java.util.Map]
 		     [isJobProcessing [Object] boolean]
 		     [isJobFinished [Object] boolean]
-		     [isJobFailed [Object] boolean]])
+		     [isJobFailed [Object] boolean]
+		     [output [java.util.Map] java.util.Map]
+		     [label [java.util.Map] String]
+		     [url [java.util.Map] String]])
 
 
 (defn -init [api-key]
+  (use 'zenclient.core)
   [[] api-key])
+
+(defn+key -eval [this sexpr] (eval (read-string sexpr)))
 
 (defn -createJob
   ([this input] (-createJob this input ""))
@@ -30,9 +38,13 @@
        (let [opts (read-string (str "(" options ")"))]
 	 (apply create-job! input opts)))))
 
-(defn+key -id [this job] (id job))
+(defn+key -id [this any] (id any))
+(defn+key -state [this any] (state any))
 (defn+key -notification [this raw] (notification raw))
 (defn+key -job [this notification] (job notification))
 (defn+key -isJobProcessing [this job] (->> job processing?))
 (defn+key -isJobFinished [this job] (->> job finished?))
 (defn+key -isJobFailed [this job] (->> job failed?))
+(defn+key -output [this notification] (:output notification))
+(defn+key -label [this output] (label output))
+(defn+key -url [this output] (url output))
